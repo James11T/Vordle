6d024e87b0e53fcaf05602fab632c98f9bb9354e
James11T <jamail090999@gmail.com>
2022-02-07 12:18:31 +0000

    added reset and load

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import {
   backspaceString,
   matchString,
 } from "./utils";
-import words from "./words";
+import guessWords from "./words/guessWords.bak";
+import dictWords from "./words/dictionaryWords.bak";
 
 import styles from "./App.module.css";
 
 const WORDLENGTH = 5;
 const GUESSLIMIT = 6;
 
-const defaultState = {
+const getDefaultState = () => ({
   word: "",
   input: "",
   guessCount: 0,
@@ -29,10 +30,19 @@ const defaultState = {
     },
     GUESSLIMIT
   ),
+  letterState: {},
+});
+
+const processLetterMatch = (letterState, input, match) => {
+  let newLetterState = { ...letterState };
+  match.forEach((matchLevel, index) => {
+    let currentLevel = newLetterState[input[index]] || 0;
+    newLetterState[input[index]] = Math.max(matchLevel, currentLevel);
+  });
+  return newLetterState;
 };
 
 const setWord = (state, word) => {
-  console.log(word);
   return { ...state, word: word.toUpperCase() };
 };
 
@@ -43,7 +53,7 @@ const submitGuess = (state) => {
   if (state.input.length !== WORDLENGTH) {
     return { ...state, error: "Please fill all blank spaces." };
   }
-  if (!words.includes(state.input)) {
+  if (!dictWords.includes(state.input)) {
     return { ...state, error: "Not a valid word." };
   }
 
@@ -58,13 +68,18 @@ const submitGuess = (state) => {
     error: "",
     guessCount: Math.min(state.guessCount + (isMatch ? 0 : 1), 5),
     canGuess: state.guessCount < 5 && !isMatch,
+    letterState: processLetterMatch(
+      state.letterState,
+      state.input,
+      matchResult
+    ),
   };
 };
 
 const pushInput = (state, input) => {
+  if (!state.canGuess) return state;
   if (!isAlphabeticalChar(input)) return state;
   if (state.input.length === 5) return state;
-  if (!state.canGuess) return state;
 
   let newInput = state.input + input.toUpperCase();
   let guesses = [...state.guesses];
@@ -80,13 +95,22 @@ const backspace = (state) => {
   return { ...state, input: newInput, guesses: guesses };
 };
 
+const dumpState = (state) => {
+  const stateJson = JSON.stringify(state);
+  localStorage.setItem("gameState", stateJson);
+};
+
+const loadJsonState = (jsonState) => {
+  return JSON.parse(jsonState);
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "setWord": {
       return setWord(state, action.word);
     }
     case "setRandomWord": {
-      return setWord(state, randomChoice(words));
+      return setWord(state, randomChoice(guessWords));
     }
     case "submitGuess": {
       return submitGuess(state);
@@ -97,6 +121,19 @@ const reducer = (state, action) => {
     case "backspace": {
       return backspace(state);
     }
+    case "dumpState": {
+      dumpState(state);
+      return state;
+    }
+    case "resetGame": {
+      let newState = getDefaultState();
+      newState.word = randomChoice(guessWords);
+      dumpState(newState);
+      return newState;
+    }
+    case "loadJsonState": {
+      return loadJsonState(action.jsonState);
+    }
     default: {
       throw new Error();
     }
@@ -104,11 +141,21 @@ const reducer = (state, action) => {
 };
 
 const App = () => {
-  const [gameState, dispatch] = useReducer(reducer, defaultState);
+  const [gameState, dispatch] = useReducer(reducer, getDefaultState());
 
   useEffect(() => {
-    dispatch({ type: "setRandomWord" });
+    let savedState = localStorage.getItem("gameState");
+    if (savedState) {
+      dispatch({ type: "loadJsonState", jsonState: savedState });
+    } else {
+      dispatch({ type: "setRandomWord" });
+    }
   }, []);
+
+  const { word } = gameState;
+  useEffect(() => {
+    console.log(word);
+  }, [word]);
 
   const handleKeyPress = useCallback(
     (key) => {
@@ -119,10 +166,15 @@ const App = () => {
 
   const handleOnSubmit = () => {
     dispatch({ type: "submitGuess" });
+    dispatch({ type: "dumpState" });
   };
 
   const handleOnBackspace = () => {
     dispatch({ type: "backspace" });
+  };
+
+  const handleResetOnClick = () => {
+    dispatch({ type: "resetGame" });
   };
 
   return (
@@ -131,11 +183,15 @@ const App = () => {
         guesses={gameState.guesses}
         focusRow={gameState.guessCount}
       />
+      <button className={styles.resetButton} onClick={handleResetOnClick}>
+        Reset
+      </button>
       <div className={styles.error}>{gameState.error}</div>
       <Keyboard
         onKeyPress={handleKeyPress}
         onSubmit={handleOnSubmit}
         onBackspace={handleOnBackspace}
+        highlights={gameState.letterState}
       />
     </>
   );
